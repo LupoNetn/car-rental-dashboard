d73f818d8f327ae167b62f15077ff33bda12c48c
oluwatobi <luponet07@gmail.com>
2025-08-16 16:30:16 -0400

    sidebar done

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar'
 
 const MainLayout = () => {
   return (
     <>
+    <div className='flex gap-4 items-baseline'>
      {/* Sidebar and Mobile Nav */}
-
+      <nav>
+        <div>
+            <Sidebar />
+        </div>
+      </nav>
      {/* Main Content */}
-     <main>
+     <main className=''>
        <Outlet />
      </main>
+     </div>
     </>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Home, User, Calendar, Bell, Settings } from "lucide-react";
+import { Link } from "react-router-dom";
+import Frame from '../assets/Frame.png'
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  // Sidebar items (array of objects with path)
+  const menuItems = [
+    { id: 1, icon: <Home size={20} />, text: "Dashboard", path: "/" },
+    { id: 2, icon: <User size={20} />, text: "Driver", path: "/driver" },
+    { id: 3, icon: <Calendar size={20} />, text: "Bookings", path: "/bookings" },
+    { id: 4, icon: <Bell size={20} />, text: "Notifications", path: "/notifications" },
+    { id: 5, icon: <Settings size={20} />, text: "Settings", path: "/settings" },
+  ];
 
-export default Sidebar
+  return (
+    <div className="md:block w-54 h-screen bg-gray-950 text-white p-4">
+     <div className="flex gap-2 items-center mb-8">
+        <img src={Frame} alt="" />
+        <h1>CAR RENT</h1>
+     </div>
+
+      {/* Map over the items */}
+      <ul className="space-y-4">
+        {menuItems.map((item) => (
+          <li key={item.id}>
+            <Link
+              to={item.path}
+              className="flex items-center gap-3 p-2 rounded-lg hover:bg-gray-700 cursor-pointer"
+            >
+              {item.icon}
+              <span>{item.text}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <hr className="mt-5 text-gray-500"/>
+    </div>
+  );
+};
+
+export default Sidebar;
